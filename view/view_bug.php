1dd839651188ace16b67d1b5801cca8da9740333
Austin-Buchanan <austinbuchanan233@gmail.com>
2022-03-29 08:35:08 -0500

    Added more search functions, drafted a single bug view.

---
+++ view/view_bug.php
@@ -0,0 +1,13 @@
+<?php include './header.php'; ?>
+<main>
+   <h1>Bug View</h1>
+   <p>Bug ID:</p>
+   <p>Software Name:</p>
+   <p>Urgency:</p>
+   <p>Short Description:</p>
+   <p>Long Description:</p>
+   <p>Time Created:</p>
+   <p>Time Modified:</p>
+   <p>Resolution:</p> 
+</main>
+<?php include './footer.php'; ?>
